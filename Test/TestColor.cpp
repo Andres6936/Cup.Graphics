055ae23d9dae07d48344f647be26f50f8e534623
Joan Andrés <andres6936@live.com>
2021-02-19 18:51:07 -0500

    Use of CPM for download the dependencies

--- Test/TestColor.cpp
+++ Test/TestColor.cpp
@@ -1,27 +1,28 @@
 // Joan Andrés (@Andres6936) Github.
 
-#include <Cup/Object/Color.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include <cassert>
+#include "doctest/doctest.h"
+#include <Cup/Object/Color.hpp>
 
 using namespace Cup::Graphics;
 
-int main (int argc, char* argv[])
+TEST_CASE("Verify the behavior of setters and getters")
 {
 	Color color(233, 245, 132);
 
-	assert(color.getRed() == 233);
-	assert(color.getGreen() == 245);
-	assert(color.getBlue() == 132);
-	assert(color.getAlpha() == 255);
+	CHECK(color.getRed() == 233);
+	CHECK(color.getGreen() == 245);
+	CHECK(color.getBlue() == 132);
+	CHECK(color.getAlpha() == 255);
 
 	color.setRed(56);
 	color.setGreen(87);
 	color.setBlue(156);
 	color.setAlpha(69);
 
-	assert(color.getRed() == 56);
-	assert(color.getGreen() == 87);
-	assert(color.getBlue() == 156);
-	assert(color.getAlpha() == 69);
+	CHECK(color.getRed() == 56);
+	CHECK(color.getGreen() == 87);
+	CHECK(color.getBlue() == 156);
+	CHECK(color.getAlpha() == 69);
 }
