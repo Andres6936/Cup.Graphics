bc71bd485d4b59dc051a460c72b59a6670ee0550
Joan Andrés <andres6936@live.com>
2021-02-19 19:06:17 -0500

    Added documentation

--- Include/Cup/Object/Color.hpp
+++ Include/Cup/Object/Color.hpp
@@ -8,6 +8,23 @@
 namespace Cup::Graphics
 {
 
+	/**
+	 * Each color is formed by mixing three basic colors: red, green and blue,
+	 * using the RGB (Red-Green-Blue) system to specify colors. When identifying
+	 * a particular color (e.g., deep white), it is defined as the sum of the
+	 * three basic colors and the proportion of the mixture of these colors.
+	 * This proportion is specified using one byte (range 0 to 255), so that
+	 * the most intense pure white (mixture of all colors) would be the color
+	 * (255,255,255), while black (absence of color) would be (0,0,0).
+	 *
+	 * Whenever a color is specified, it is specified as a triplet (R,G,B) that
+	 * defines how much red (R value), green (G) and blue (B) make up that
+	 * color. These three values are called RGB components of the color. Thus, a
+	 * medium intense red could be defined as (128,0,0) (128 of red and none of
+	 * the other 2 components). In this way (mixing these three components with
+	 * the different intensities 0-255) we can obtain up to 256*256*256 = 16.7
+	 * million different colors.
+	 */
 	class Color
 	{
 
